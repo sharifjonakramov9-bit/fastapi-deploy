f7050b5e35ef067cc735a82ddb7ae6bf54fb9e7c
djumanov <djumanovdev@gmail.com>
2026-03-27 10:36:52 +0500

    fastapi upload file to s3

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
-from fastapi import FastAPI
+import asyncio
+import os
+from uuid import uuid4
 
-app = FastAPI(title="FastAPI deploy")
+import boto3
+from fastapi import FastAPI, UploadFile
+from dotenv import load_dotenv
+
+load_dotenv()
+app = FastAPI()
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("aws_access_key_id"),
+    aws_secret_access_key=os.getenv("aws_secret_access_key"),
+    region_name="eu-north-1",
+    endpoint_url="https://s3.eu-north-1.amazonaws.com",
+)
+BUCKET = "nt-sn03-bucket"
 
 
-@app.get("/")
-async def home_view():
-    return {"message": "hello world"}
+@app.post("/api/upload")
+async def upload_file(image: UploadFile):
+    key = f"test/{uuid4()}.pdf"
 
+    image.file.seek(0)
 
-@app.get("/about")
-async def about_view():
-    return {"message": "about page"}
+    loop = asyncio.get_running_loop()
+
+    await loop.run_in_executor(
+        None,
+        lambda: s3.upload_fileobj(
+            image.file,
+            BUCKET,
+            key,
+            ExtraArgs={
+                "ContentType": image.content_type,
+                "ContentDisposition": "inline",
+            },
+        ),
+    )
+
+    url = s3.generate_presigned_url(
+        "get_object", Params={"Bucket": BUCKET, "Key": key}, ExpiresIn=3600
+    )
+
+    return {"key": key, "url": url}
